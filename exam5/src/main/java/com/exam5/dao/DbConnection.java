d091e9211c4d1834448f63120e4e605e42710004
ezdes <ermekxt22@gmail.com>
2021-03-20 17:30:56 +0600

    done

---
+++ exam5/src/main/java/com/exam5/dao/DbConnection.java
@@ -0,0 +1,22 @@
+package com.exam5.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/codingbat";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "2m3410lt";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+}
